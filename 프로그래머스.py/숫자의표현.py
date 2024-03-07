0e446735c3c3967bea3955f4b5e66b27e472610f
KyoungHyun <102707496+namsta9ramm@users.noreply.github.com>
2024-03-07 16:58:29 +0900

    20240307 solve algorithm

---
+++ 프로그래머스.py/숫자의표현.py
@@ -0,0 +1,11 @@
+def solution(n):
+    answer=0
+    listans=[ i for i in range(1,n+1)]
+    for j in range(1,n+1):
+        for k in range(1+n-j):
+            if sum(listans[k:k+j])==n:
+                answer=answer+1
+                break
+    return answer
+# 1, 2 ,3 ,4 ,5 ,6 ,,
+print(solution(15))
